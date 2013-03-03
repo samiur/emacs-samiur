862ba7dde35995d2e50323b04b0a56e4d0f73c83
Samiur Rahman <samiur.rahman@synapse.com>
2013-03-03 12:26:29 -0800

    Adding inf-ruby

---
+++ el-get/.loaddefs.el
@@ -0,0 +1,16 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("inf-ruby/inf-ruby.el") (20787 36648 931182))
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
---
+++ el-get/.status.el
@@ -0,0 +1,2 @@
+((inf-ruby status "installed" recipe
+           (:name inf-ruby :type http :description "Inferior Ruby Mode - ruby process in a buffer." :url "http://bugs.ruby-lang.org/projects/ruby-trunk/repository/raw/misc/inf-ruby.el")))
